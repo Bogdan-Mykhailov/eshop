f9213cb576279d77020b7915522de223fa6f49ca
Bogdan <bogdan_mykhailov@icloud.com>
2022-08-24 23:28:39 +0300

    :lipstick: edit header component, add styles to header

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.module.css';
+import s from './App.module.css'
+import {Header} from "../Header/Header";
+
+
+function App() {
+  return (
+    <div className={s.app}>
+      <Header/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import s from "./Header.module.css";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+export const Header = () => {
+
+  return (
+    <div className={s.header}>
+      <div className={s.headerLogo}>
+        <StorefrontIcon className={s.headerLogoImage} fontSize="large"/>
+        <h2 className={s.headerLogoTitle}>eShop</h2>
+      </div>
+      <div className={s.headerSearch}>
+        <input type="text" className={s.headerSearchInput}/>
+        <SearchIcon className={s.headerSearchIcon}/>
+      </div>
+      <div className={s.headerNav}>
+        <div className={s.navItem}>
+          <span className={s.navItemLineOne}>Hello Guest</span>
+          <span className={s.navItemLineTwo}>Sing In</span>
+        </div>
+        <div className={s.navItem}>
+          <span className={s.navItemLineOne}>Your</span>
+          <span className={s.navItemLineTwo}>Shop</span>
+        </div>
+        <div className={s.navItem}>
+          <ShoppingCartIcon className={s.itemCart}/>
+          <span className={s.navItemLineTwo + ' ' + s.cartCount}>0</span>
+        </div>
+      </div>
+    </div>
+  );
+};
